352814b94172434e90099a91dcbffb3dcfed8d05
HeeDragoN1123 <yhy7952@gmail.com>
2023-09-12 15:51:26 +0900

    중간 저장 포스트 컨트롤러 / 포스트 서비스 시작

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,1 +1,75 @@
-export class PostsController {}
+import { PostsService } from "../services/signupService.js";
+export class PostsController {
+    postsService = new PostsService();
+
+/* 게시글 생성 API */
+
+createPost = async (req, res ,next) =>{
+ try{
+    const {nickname , userId} = req.user
+    const {title , content } = req.body
+
+    const createdPost = await this.createPost({
+        UserId: userId,
+        nickname,
+        title,
+        content,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+  });
+
+  return res.status(200).json({data : createdPost});
+
+    }catch(err){
+        next(err);
+    }
+    
+}
+
+
+/* 게시글 조회 API */
+
+getPosts = async (req,res ,next) =>{
+try{
+    const posts = await this.postsService.findAllPost();
+
+    return res.status(200).json({data : posts});
+}catch(err){
+    next(err);
+}
+
+}
+
+/* 게시글 상세 조회 API */
+
+getOnePost = async (req,res ,next) =>{
+    try{
+        const { postId } = req.params;
+
+        const post = await this.postsService.findOnePost();
+    
+        return res.status(200).json({data : post});
+
+    }catch(err){
+        next(err);
+    }
+    
+    }
+
+
+/* 게시글 수정 API */
+
+
+putPost = async (req,res ,next) =>{
+
+}
+
+
+/* 게시글 삭제 API */
+
+deletePost = async (req,res ,next) =>{
+
+}
+
+
+}
--- src/routes/post.js
+++ src/routes/post.js
@@ -5,15 +5,17 @@ const router = express.Router();
 const postsController = new PostsController(); // 포스트 컨트롤러를 인스턴스 화 시킴
 
 /* 게시글 생성 API */
-
-
+router.post('/posts', postsController.createPost);
 /* 게시글 조회 API */
-
+router.get('/posts' , postsController.getPosts);
 /* 게시글 상세 조회 API */
-
+router.get('/posts/:postId', postsController.getOnePost);
 /* 게시글 수정 API */
-
+router.put('/posts/:postId', postsController.putPost);
 /* 게시글 삭제 API */
+router.delete('/posts/:postId', postsController.deletePost);
+
+
 
 export default router;
 
